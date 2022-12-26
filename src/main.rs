9f408bf04cfec9ff26ff9973f858f14e5d8828d7
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-26 17:10:46 -0600

    Feature: Added Actix boilerplate
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_session::storage::CookieSessionStore;
+use actix_session::SessionMiddleware;
+use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer};
+use env_logger::Env;
+
+mod routes;
+
+static SESSION_SIGNING_KEY: &[u8] = &[0; 64]; // Just an example
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let key = actix_web::cookie::Key::from(SESSION_SIGNING_KEY);
+
+    let host = "0.0.0.0";
+    let port = 8080;
+    log::info!("Serving on {} port {}", host, port);
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
+                    .cookie_secure(false)
+                    .build(),
+            )
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .service(routes::hello)
+            .service(Files::new("/images", "static/images/").show_files_listing())
+            .service(Files::new("/", "./static/root/").index_file("index.html"))
+    })
+    .bind((host, port))?
+    .run()
+    .await
 }
