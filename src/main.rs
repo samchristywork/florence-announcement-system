b4a12a5d037dcc6ea8e6483da9bd98cd7e3fa731
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-29 17:28:00 -0600

    Feature: Added routes and app data to main
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use actix_files::Files;
 use actix_session::storage::CookieSessionStore;
 use actix_session::SessionMiddleware;
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
-use env_logger::Env;
+use actix_web::{web, App, HttpServer};
+use r2d2_sqlite::SqliteConnectionManager;
+use std::sync::Arc;
 
 mod announcement;
 mod routes;
@@ -12,25 +13,39 @@ static SESSION_SIGNING_KEY: &[u8] = &[0; 64]; // Just an example
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let connection = sqlite::open("data.sqlite").unwrap();
+    let sqlite_file = "default.db";
+    let sqlite_connection_manager = SqliteConnectionManager::file(sqlite_file);
+
+    let sqlite_pool = r2d2::Pool::new(sqlite_connection_manager)
+        .expect("Failed to create SQLite connection from file.");
 
     let query = "
-    create table if not exists users (name text, age integer);
-    insert into users values ('Alice', 42);
-    insert into users values ('Bob', 69);
-    insert into users values ('Charlie', 69);
-";
+    create table if not exists announcements (
+        status text,
+        created text,
+        scheduled text,
+        title text,
+        body text,
+        id text
+    );
+    ";
 
-    connection.execute(query).unwrap();
+    sqlite_pool.get().unwrap().execute_batch(query).unwrap();
 
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let pool_arc = Arc::new(sqlite_pool);
+
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
     let key = actix_web::cookie::Key::from(SESSION_SIGNING_KEY);
 
     let host = "0.0.0.0";
     let port = 8080;
+
     log::info!("Serving on {} port {}", host, port);
+
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool_arc.clone()))
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
                     .cookie_secure(false)
@@ -39,7 +54,10 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .service(routes::hello)
-            .service(routes::announcements)
+            .service(routes::announcements_list)
+            .service(routes::announcements_add)
+            .service(routes::announcements_delete)
+            .service(routes::announcements_update)
             .service(routes::rss)
             .service(routes::active)
             .service(Files::new("/images", "static/images/").show_files_listing())
