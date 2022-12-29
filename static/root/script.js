9113a38b0da5e75729aca54c45ffdb6a7a78af29
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-28 20:13:45 -0600

    Feature: Add callback func for adding announcement
    Contact: Sam Christy <samchristywork@gmail.com>

--- static/root/script.js
+++ static/root/script.js
@@ -8,3 +8,33 @@ fetch("./announcements/list")
     published.innerHTML = data
   });
 
+function add_announcement() {
+  let title = window.prompt("Please enter a title for the announcement:", "Some Title");
+  let body = window.prompt("Please enter an annoucement body:", "Some Body");
+
+  var d = new Date();
+  const options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          "title": title,
+          "body": body,
+          "created": d.toLocaleString('en-US', { timeZone: 'America/Chicago' }) + " CT",
+          "scheduled": "--",
+          "id": Math.random().toString(16).substr(2, 8),
+          "status": "neutral",
+        }
+      )
+  };
+
+  fetch('./announcements/add', options)
+      .then(response => response.text())
+      .then(response => {
+        location.reload();
+      });
+}
+
