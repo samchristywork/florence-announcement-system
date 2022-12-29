50136e2d7aef097b6fd8984ab68c18e15a147ec3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-29 16:34:31 -0600

    Feature: Added route to list announcements
    This route grabs all of the announcemnts from the SQLite table and
    formats them with HTML so that they can be displayed on the main page.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/routes.rs
+++ src/routes.rs
@@ -1,23 +1,63 @@
 use crate::announcement::Announcement;
-use actix_web::{get, web, Responder};
-use sqlite::State;
+use actix_web::{get, post, web, Responder};
+use serde::Deserialize;
+use std::sync::Arc;
 
 #[get("/hello/{name}")]
 async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[get("/announcements/list")]
+async fn announcements_list(
+    pool: web::Data<Arc<r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>>>,
+) -> impl Responder {
     let mut ret = String::new();
 
-    let connection = sqlite::open("data.sqlite").unwrap();
+    let mut count = 0;
+    loop {
+        match pool.get().unwrap().query_row(
+            "select * from announcements limit 1 offset ?",
+            [count],
+            |row| {
+                let id = row.get::<usize, String>(5).unwrap();
 
-    let query = "select * from users where age > ?";
-    let mut statement = connection.prepare(query).unwrap();
-    statement.bind((1, 50)).unwrap();
+                ret += format!(
+                    "<div class='announcement-{}'>
+        <div class='date'>
+          <div>Created: {}</div>
+          <div>Scheduled: {}</div>
+        </div>
+        <div class='title' onclick='update_announcement(\"{id}\", \"title\", \"{}\")'>{}</div>
+        <div class='body' onclick='update_announcement(\"{id}\", \"body\", \"{}\")'>{}</div>
+        <button style='color:green' onclick='set_state(\"{id}\", \"approved\")'>Approve</button>
+        <button style='color:#770'>Schedule</button>
+        <button style='color:red' onclick='set_state(\"{id}\", \"denied\")'>Deny</button>
+        <button style='color:maroon' onclick='delete_announcement(\"{id}\")'>Delete</button>
+        <button style='color:blue'>Hide</button>
+        <div class='id'>{id}</div>
+      </div>",
+                    row.get::<usize, String>(0).unwrap(), // status
+                    row.get::<usize, String>(1).unwrap(), // created
+                    row.get::<usize, String>(2).unwrap(), // scheduled
+                    row.get::<usize, String>(3).unwrap(), // title
+                    row.get::<usize, String>(3).unwrap(), // title
+                    row.get::<usize, String>(4).unwrap(), // body
+                    row.get::<usize, String>(4).unwrap(), // body
+                )
+                .as_str();
 
-    while let Ok(State::Row) = statement.next() {
-        ret += format!("name = {}", statement.read::<String, _>("name").unwrap()).as_str();
-        ret += format!("age = {}", statement.read::<i64, _>("age").unwrap()).as_str();
+                Ok(())
+            },
+        ) {
+            Ok(_) => {}
+            Err(_) => break,
+        }
+
+        count += 1;
+
+        ()
     }
-
-    //format!("Hello {name}!")
     ret
 }
 
