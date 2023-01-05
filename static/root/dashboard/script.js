e91a8c593a062103f2c1b7b8dc202d6d6edcde50
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-04 19:26:56 -0600

    Feature: List all announcements on public page
    Contact: Sam Christy <samchristywork@gmail.com>

--- static/root/dashboard/script.js
+++ static/root/dashboard/script.js
@@ -1,4 +1,12 @@
+let announcements = document.querySelector("#announcements");
+
 document.querySelector("#checkbox")
   .addEventListener("change", function(e) {
     console.log(this.checked)
+  });
+
+fetch("./all/")
+  .then(response => response.json())
+  .then(data => {
+    announcements.innerHTML=JSON.stringify(data);
   });
