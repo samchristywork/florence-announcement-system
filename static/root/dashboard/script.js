753d59872ca07e62ae5d3adbd4679dc3eec3e0b4
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-05 18:36:59 -0600

    Feature: Render public facing announcements
    This renders public facing announcements on the client side. This is
    different from the server side rendering because I want to be able to
    render these announcements on any client that can read JSON.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- static/root/dashboard/script.js
+++ static/root/dashboard/script.js
@@ -8,5 +8,23 @@ document.querySelector("#checkbox")
 fetch("./all/")
   .then(response => response.json())
   .then(data => {
-    announcements.innerHTML=JSON.stringify(data);
+
+    let html="";
+
+    for (let x of data) {
+
+      html+=`<div class='announcement-public'>
+        <div class='date'>
+          <div>Created: ${x.created}</div>
+        </div>
+        <div style='display: grid; grid-template-columns: 1fr 1fr'>
+          <div class='title'>${x.title}</div>
+        </div>
+        <div class='body'>${x.body}</div>
+      </div>`
+    }
+
+    html+="<hr/>";
+
+    announcements.innerHTML=html;
   });
